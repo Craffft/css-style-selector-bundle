ce8088304caf76ac18f119183920645362fe665e
fritzmg <fmg@inspiredminds.at>
2017-08-04 14:05:30 +0200

    add english translations

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the CssStyleSelector Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC']['cssStyleSelector'] = array('CSS style selection', 'You can select the CSS styles with the predefined CSS classes here.');
---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the CssStyleSelector Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['cssStyleSelector'] = array('CSS styles', 'You can define CSS styles with predefined CSS classes here.');
