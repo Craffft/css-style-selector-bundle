6b0d9de3f97f97d4248d82381b446e48c29da114
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-09-27 11:04:23 +0100

    fix warning

--- src/Resources/contao/dca/tl_css_style_selector.php
+++ src/Resources/contao/dca/tl_css_style_selector.php
@@ -83,7 +83,7 @@ $GLOBALS['TL_DCA']['tl_css_style_selector'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_css_style_selector']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\'))return false;Backend.getScrollOffset()"'
             ),
             'show' => array
             (
